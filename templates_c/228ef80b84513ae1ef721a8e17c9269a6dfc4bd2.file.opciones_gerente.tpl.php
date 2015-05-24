faad46c4d0523cd5580be4a6f2740131ede3ad11
estefani <estefani@PERSONAL>
2015-05-24 17:11:03 -0500

    Commit CU11-CalificarPrediseno Listo1

--- templates_c/228ef80b84513ae1ef721a8e17c9269a6dfc4bd2.file.opciones_gerente.tpl.php
+++ templates_c/228ef80b84513ae1ef721a8e17c9269a6dfc4bd2.file.opciones_gerente.tpl.php
@@ -1,17 +1,17 @@
-<?php /* Smarty version Smarty-3.0.9, created on 2015-05-23 23:49:09
+<?php /* Smarty version Smarty-3.0.9, created on 2015-05-25 00:03:28
          compiled from "C:/wamp/www/ProyectoDCPS/templates\opciones_gerente.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:174455560f5d55a7f26-37830944%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:123655624ab096b265-27769769%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     '228ef80b84513ae1ef721a8e17c9269a6dfc4bd2' => 
     array (
       0 => 'C:/wamp/www/ProyectoDCPS/templates\\opciones_gerente.tpl',
-      1 => 1432080296,
+      1 => 1432504999,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '174455560f5d55a7f26-37830944',
+  'nocache_hash' => '123655624ab096b265-27769769',
   'function' => 
   array (
   ),
@@ -24,5 +24,5 @@ $_smarty_tpl->decodeProperties(array (
         <br/><a href="cu2-proponeridea.php"><div class="item">Proponer Idea</div></a><br/><br/>
         <a href="cu3-calificaridea.php"><div class="item">Calificar Idea</div></a><br/><br/>
         <a href="cu4-Programar_reunion.php"><div class="item">Programar Reunion</div></a><br/><br/>
-        <a href="cu11-calificar_prediseno.php"><div class="item">Calificar Prediseno</div></a><br/>
+        <a href="cu11-calificarprediseno.php"><div class="item">Calificar Prediseno</div></a><br/>
     </div>
